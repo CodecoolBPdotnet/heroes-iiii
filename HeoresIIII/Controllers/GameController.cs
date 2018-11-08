a766de9d306c361f806d77922be822c1ee68c53b
SinyiMark <35994165+SinyiMark@users.noreply.github.com>
2018-11-08 09:43:44 +0100

    GameController created and Game.js

--- HeoresIIII/Controllers/GameController.cs
+++ HeoresIIII/Controllers/GameController.cs
@@ -2,14 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HeroesIIII.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class GameController : ControllerBase
+    public class GameController : Controller
     {
+        public IActionResult Index()
+        {
+            return View();
+        }
     }
 }
---
+++ HeoresIIII/wwwroot/js/Game.js
@@ -0,0 +1,16 @@
+﻿window.addEventListener("load", this.LoadHero());
+
+function LoadHero() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = this.ShowHero();
+    xhttp.open("GET", "Hero/0", true);
+    xhttp.send();
+}
+
+function ShowHero() {
+    if (this.readyState == 4 && this.status == 200)
+    {
+        var myHero = this.responseText;
+        document.write(myHero);
+    }
+}
