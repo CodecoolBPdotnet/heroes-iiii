d769305e09c6b1e29ae03d31284a906b92902e17
David Leblay <leblaydavid7@gmail.com>
2018-11-08 13:23:20 +0100

    Refactored the game page

--- HeoresIIII/wwwroot/js/Game.js
+++ HeoresIIII/wwwroot/js/Game.js
@@ -2,21 +2,21 @@
 
 function LoadHero() {
     var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
+    xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            var hero = JSON.parse( this.responseText);
-            console.log(hero);
-            document.getElementById("heroname").innerHTML = hero["name"] + " | ";
-            document.getElementById("hp").innerHTML += hero["currentHealth"];
+            var hero = JSON.parse(this.responseText);
+            console.log(hero);  // TODO remove later
+            document.getElementById("name").innerHTML = hero["name"];
+            document.getElementById("hp").innerHTML = hero["currentHealth"];
             document.getElementById("level").innerHTML = "Level: " + hero["level"];
-            document.getElementById("skillpoint").innerHTML = "Skillponit: " + hero["skillPoints"];
+            document.getElementById("skillpoint").innerHTML = "Skillpoints: " + hero["skillPoints"];
             document.getElementById("atk").innerHTML = hero["damage"];
             document.getElementById("def").innerHTML = hero["defense"];
             document.getElementById("agi").innerHTML = hero["agility"];
             document.getElementById("vit").innerHTML = hero["vitality"];
 
-    }
-  };
+        }
+    };
     xhttp.open("GET", "api/Hero/1", true);
     xhttp.send();
 }
