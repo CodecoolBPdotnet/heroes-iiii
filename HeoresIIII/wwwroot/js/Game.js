4c24f8712ce362696fb2131f501842bfdf17f842
SinyiMark <35994165+SinyiMark@users.noreply.github.com>
2018-11-08 11:14:37 +0100

    js ajax show hero finish without true design

--- HeoresIIII/wwwroot/js/Game.js
+++ HeoresIIII/wwwroot/js/Game.js
@@ -4,7 +4,17 @@ function LoadHero() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById
+            var hero = JSON.parse( this.responseText);
+            console.log(hero);
+            document.getElementById("heroname").innerHTML = hero["name"] + " | ";
+            document.getElementById("hp").innerHTML += hero["currentHealth"];
+            document.getElementById("level").innerHTML = "Level: " + hero["level"];
+            document.getElementById("skillpoint").innerHTML = "Skillponit: " + hero["skillPoints"];
+            document.getElementById("atk").innerHTML = hero["damage"];
+            document.getElementById("def").innerHTML = hero["defense"];
+            document.getElementById("agi").innerHTML = hero["agility"];
+            document.getElementById("vit").innerHTML = hero["vitality"];
+
     }
   };
     xhttp.open("GET", "api/Hero/1", true);
